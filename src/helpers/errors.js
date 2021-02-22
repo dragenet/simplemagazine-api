38074992e858f367354452a572fac568b9ca15a5
Dominik 'Dragenet' Domagalski <dragenetdev@gmail.com>
2021-02-22 15:29:23 +0100

    Change errors code convention

--- src/helpers/errors.js
+++ src/helpers/errors.js
@@ -2,17 +2,17 @@ import httpStat from './httpStatus'
 
 export default {
   default: {
-    code: 'UNKNOW_ERROR',
+    code: 'ERR_UNKNOW_ERROR',
     message: 'Unknow error',
     httpStatus: httpStat.internalError,
   },
   userExists: {
-    code: 'USER_ALREADY_EXISTS',
+    code: 'ERR_USER_ALREADY_EXISTS',
     message: 'User with specified email already exists',
     httpStatus: httpStat.badRequest,
   },
   incorrectEmailOrPassword: {
-    code: 'EMAIL_OR_PASS_INCORR',
+    code: 'ERR_EMAIL_OR_PASS_INCORR',
     message: "Email/name doesn't exists or password is incorrect",
     httpStatus: httpStat.badRequest,
   },
