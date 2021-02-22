fdb15d95c792c22d400ea913097bbfcefbf28056
Dominik 'Dragenet' Domagalski <dragenetdev@gmail.com>
2021-02-22 13:17:26 +0100

    Add token generation

---
+++ src/helpers/token.js
@@ -0,0 +1,73 @@
+import jwt from 'jsonwebtoken'
+import crypto from 'crypto'
+
+export const token_types = {
+  auth: 'auth',
+  refresh: 'refresh',
+}
+
+const isTypeValid = type => {
+  if (type !== token_types.auth && type !== token_types.refresh)
+    throw Error('Token type unspecified')
+}
+
+const evalSecret = secret => {
+  let jwtSecret
+  if (secret && secret !== null) {
+    jwtSecret = secret
+  } else {
+    jwtSecret = process.env.JWT_SECRET
+  }
+  return jwtSecret
+}
+
+const evalExpTime = tokenType =>
+  process.env[`${tokenType.toUpperCase()}_TOKEN_EXPIRATION_TIME`]
+
+export const genPayload = (data, tokenType) => {
+  let payload = {
+    type: tokenType,
+    data,
+  }
+
+  payload.iat = Date.now()
+
+  let randomizedPayload = Object.assign({}, payload)
+  randomizedPayload.random = crypto.randomBytes(256)
+  payload.tokenId = crypto
+    .createHash('sha256')
+    .update(JSON.stringify(randomizedPayload))
+    .digest('hex')
+
+  return payload
+}
+
+export const genToken = (type, data, options, secret) => {
+  isTypeValid(type)
+
+  const jwtSecret = evalSecret(secret)
+
+  const payload = genPayload(data, type)
+
+  const opts = {
+    expiresIn: evalExpTime(type),
+    jwtid: payload.tokenId,
+    ...options,
+  }
+
+  return [payload.tokenId, jwt.sign(payload, jwtSecret, opts)]
+}
+
+export const verifyToken = (type, token, options, secret) => {
+  isTypeValid(type)
+
+  const jwtSecret = evalSecret(secret)
+
+  const opts = {
+    algorithms: ['HS256'],
+    clockTolerance: 10,
+    ...options,
+  }
+
+  return jwt.verify(token, jwtSecret, opts)
+}
