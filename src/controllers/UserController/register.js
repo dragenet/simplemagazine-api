a4e756f5d1121ecfd3c3562b993198188791a736
Dominik 'Dragenet' Domagalski <dragenetdev@gmail.com>
2021-02-22 13:18:33 +0100

    Create register controller

---
+++ src/controllers/UserController/register.js
@@ -0,0 +1,41 @@
+//libs
+import wrap from 'express-async-wrapper'
+
+//models
+import db from '../../db/models'
+import User from '../../models/User'
+
+//helpers
+import ControllerError from '../../helpers/ControllerError'
+import errors from '../../helpers/errors'
+import httpStat from '../../helpers/httpStatus'
+
+const checkIsUserExists = async user => {
+  const res = await db.User.findAll({
+    where: {
+      email: user.email,
+    },
+  })
+  if (res.length !== 0) return true
+  else return false
+}
+
+export const registerUser = wrap(async (req, res) => {
+  const user = req.body
+
+  const isUserExists = await checkIsUserExists(user)
+  if (isUserExists) throw new ControllerError(errors.userExists)
+
+  const newUser = new User(user.name, user.email, user.password, null)
+
+  const createdUser = await db.User.create(newUser)
+
+  newUser.id = createdUser.id
+
+  const responseJson = {
+    message: 'User created',
+    user: newUser.get(),
+  }
+
+  res.status(httpStat.created).json(responseJson)
+})
