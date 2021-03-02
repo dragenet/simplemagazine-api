996afdb56a3222ef75c0461ca858ce186527b4cb
Dominik 'Dragenet' Domagalski <dragenetdev@gmail.com>
2021-03-02 12:19:05 +0100

    Patch user update path

--- src/controllers/UserController/updateUser.js
+++ src/controllers/UserController/updateUser.js
@@ -2,11 +2,14 @@ import wrap from 'express-async-wrapper'
 import { User } from '@/models'
 import { UserAdapter } from '@/adapters'
 import { httpStatus, responses } from '@/utils'
+import { clearTokenCookie, token_types } from '@/helpers'
 
 export const updateUser = wrap(async (req, res) => {
   if (req.body.id) delete req.body.id
+
   const newUser = new User({ ...req.user, ...req.body })
-  const updatedUser = await UserAdapter.updateUser(newUser)
+  await UserAdapter.updateUser(newUser)
+  const updatedUser = await UserAdapter.getUser(newUser)
 
   res
     .status(httpStatus.ok)
